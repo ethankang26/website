eb4c0e0c0013b233aa7f9eda6c71dc2c03f70d4e
Ethan Kang <ethankang26@gmail.com>
2025-06-03 22:14:57 -0400

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Unrepped',
   description: 'Created with v0',
-  generator: 'v0.dev',
+  generator: 'unrepped.dev',
 }
 
 export default function RootLayout({
